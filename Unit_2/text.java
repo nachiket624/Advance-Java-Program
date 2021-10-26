7f3f386a4b3a1d84ebaf2f1d50bdc0c76909fd94
Nachiket <nachiketparjane054@gmail.com>
2021-10-26 19:24:43 +0530

    Change text area

--- Unit_2/text.java
+++ Unit_2/text.java
@@ -3,7 +3,7 @@ public class text
 {  
      text(){  
         JFrame f= new JFrame();  
-        JTextArea area=new JTextArea("Welcome to javatpoint");  
+        JTextArea area=new JTextArea("Welcome to javaProgrmming");  
         area.setBounds(10,30, 200,200);  
         f.add(area);  
         f.setSize(300,300);  
